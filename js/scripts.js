92ab324ecd1cd320e9ad635d886b8cc322597da0
Evgeny Sinitsyn <workevs@yandex.ru>
2017-11-20 22:36:55 +0500

    edit third week

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,28 @@
+//
+
+var hamburger = document.querySelector('#hamburger');
+var mobileMenu = document.querySelector('#mobile-menu');
+
+hamburger.addEventListener('click', function() {
+  hamburger.classList.toggle('open');
+  mobileMenu.classList.toggle('open');
+});
+
+//
+
+var compositionButton = document.querySelectorAll('.composition__button');
+var compositionList = document.querySelectorAll('.composition__list');
+
+for (var i = 0; i < compositionButton.length; i++) {
+  compositionButton[i].addEventListener('click', function() {
+    for (var a = 0; a < compositionList.length; a++) {
+      compositionList[a].classList.toggle('open');
+    }
+  });
+};
+
+//
+
 $(document).ready(function(){
     $(".team-menu__name").click( function () {
       var container = $(this).parents(".team-menu__item");
